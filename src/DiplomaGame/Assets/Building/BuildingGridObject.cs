95826062fb328b924f9b642610ddc7285e7c0fbd
Donjkee <mr142maksm@gmail.com>
2025-02-06 13:20:04 +0200

    Fixed constructor BuildingGridObject

--- src/DiplomaGame/Assets/Building/BuildingGridObject.cs
+++ src/DiplomaGame/Assets/Building/BuildingGridObject.cs
@@ -2,14 +2,12 @@
 
 public class BuildingGridObject
 {
-	private MapGrid<BuildingGridObject> _grid;
 	private int _x;
 	private int _y;
 	private Guid _guid;
 
 	public BuildingGridObject(MapGrid<BuildingGridObject> grid, int x, int y, Guid guid = default)
 	{
-		_grid = grid;
 		_x = x;
 		_y = y;
 		_guid = guid;
